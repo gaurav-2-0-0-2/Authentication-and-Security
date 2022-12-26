5fef4c8672e9db0b18cd7efcabc92f9fb25812e8
Gaurav Tak <gauravtak996@gmail.com>
2022-12-26 18:28:09 +0530

    project completed

--- app.js
+++ app.js
@@ -39,7 +39,8 @@ mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser:true});
 const userSchema = new mongoose.Schema({
     email: String,
     password: String,
-    googleId: String
+    googleId: String,
+    secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -123,11 +124,47 @@ app.get("/register", function (req,res) {
 });
 
 app.get('/secrets', (req,res)=>{
+    User.find({"secret": {$ne: null}}, (err,foundUsers)=>{
+        if(err){
+            console.log(err);
+        }else{
+            if (foundUsers) {
+                res.render("secrets", {usersWithSecrets: foundUsers});
+            }
+        }
+    });
+   
+});
+
+
+
+
+app.get("/submit", (req,res)=>{
     if (req.isAuthenticated()) {
-        res.render("secrets");
+        res.render("submit");
     } else {
         res.redirect('/login')
     }
+});
+
+
+app.post("/submit", (req,res)=>{
+    const submittedSecret = req.body.secret
+
+    console.log(req.user.id);
+
+    User.findById(req.user.id, (err, foundUser)=>{
+        if(err){
+            console.log(err);
+        }else{
+            if(foundUser){
+                foundUser.secret = submittedSecret;
+                foundUser.save(function () {
+                    res.redirect("/secrets");
+                })
+            }
+        }
+    })
 })
 
 app.get('/logout',function(req,res,next){
@@ -136,6 +173,7 @@ app.get('/logout',function(req,res,next){
         res.redirect('/');
       });
 })
+
 
 app.post("/register", function (req, res) {
     User.register({username: req.body.username}, req.body.password, (err,user)=>{
